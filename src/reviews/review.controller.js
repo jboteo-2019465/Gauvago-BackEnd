b21e74a432462dbe532a346ce84e71607321d7e7
dzabala-2019001 <dzabala-2019001@kinal.edu.gt>
2024-04-26 11:48:29 -0600

    se realizo el register del rewiev

--- src/reviews/review.controller.js
+++ src/reviews/review.controller.js
@@ -5,3 +5,15 @@ export const test = (req, res) =>{
     console.log('test panoli')
     return res.send({message: 'test'})
 }
+
+export const register = async (req, res) => {
+    try {
+        let data = req.body;
+        let review = new Review(data);
+        await review.save();
+        return res.send({ message: 'The review has been registered' });        
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send({ message: 'Error registering the review' });
+    }
+}
--- src/reviews/review.routes.js
+++ src/reviews/review.routes.js
@@ -1,13 +1,14 @@
 'use strict'
 
 import { Router } from "express"
-import {test} from "./review.controller.js"
+import {register, test} from "./review.controller.js"
 
 // crear una nueva instancia de enrutador de express
 const api = Router() 
 
 // definir una ruta GET para el endpoint '/test'
 api.get('/test', test)
+api.post('/register', register)
 
 // exportar la instancia de enrutador como la exportación predeterminada
 export default api
