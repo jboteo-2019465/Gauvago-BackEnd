3b9c941eb91aa46f9f571ca9b340bf0e56b6958d
cdelacruz-2019262 <cdelacruz-2019262@kinal.edu.gt>
2024-05-05 20:15:13 -0600

    isAdmins de hotel

--- src/hotel/hotel.routes.js
+++ src/hotel/hotel.routes.js
@@ -1,16 +1,16 @@
 import { Router } from "express";
 import { deleteH, getHotelRequest, obtener,  registerHotel,  registerHotelRequest, searchH, test, updateH } from "./hotel.controller.js";
-import { isAdmin_AdminHotel, isHotel, validateJwt } from "../middleware/validate-jwt.js";
+import { isAdmin, isAdmin_AdminHotel, isHotel, validateJwt } from "../middleware/validate-jwt.js";
 
 const api = Router();
 
 api.get('/test', test)
-api.post('/register', registerHotel)
+api.post('/register', [isHotel, validateJwt],registerHotel)
 api.post('/request/register', registerHotelRequest)
 api.get('/obtener', obtener)
 api.put('/update/:id', [isHotel, validateJwt], updateH)
 api.delete('/delete/:id', [isAdmin_AdminHotel, validateJwt], deleteH)
 api.post('/search', searchH)
-api.get('/request/view', getHotelRequest)
+api.get('/request/view', [isAdmin, validateJwt], getHotelRequest)
 
 export default api
