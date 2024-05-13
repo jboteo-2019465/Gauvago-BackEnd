394b40ba612335420f38427f484c058465b75b6a
jboteo-2019465 <jboteo-2019465@kinal.edu.gt>
2024-05-13 08:28:21 -0600

    Arreglo de reservacion

--- src/reservation/reservation.controller.js
+++ src/reservation/reservation.controller.js
@@ -6,40 +6,42 @@ import Room from '../room/room.model.js'
 // Agregar reservacion
 export const addReserva = async (req, res) => {
     try {
-      let data = req.body
-      //let uid = req.user._id -- Chingadera no jala si la pongo
-  
-      // Hace la reservacion
-      let newReserva = new Reser(data)
-      await newReserva.save()
+        let data = req.body
+        let user = req.user
+        data.client = user._id
 
-      // Verifica si la habitacion esta disponible
-      let room = await Room.findOne({ _id: data.room, available: 'DISPONIBLE' })
-      if (!room) return res.status(404).send({ message: 'Room not found or not available' })
-  
-      // Cambiar la disponibilidad de la habitacion
-      room.available = 'NODISPONIBLE'
-      await room.save()
-  
-      return res.status(200).send({ message: 'Reservation added successfully', newReserva })
+        // Hace la reservacion
+        let newReserva = new Reser(data)
+        await newReserva.save()
+
+        // Verifica si la habitacion esta disponible
+        let room = await Room.findOne({ _id: data.room, available: 'DISPONIBLE' })
+        if (!room) return res.status(404).send({ message: 'Room not found or not available' })
+
+        // Cambiar la disponibilidad de la habitacion
+        room.available = 'NODISPONIBLE'
+        await room.save()
+
+        return res.status(200).send({ message: 'Reservation added successfully', newReserva })
     } catch (err) {
-      return res.status(500).send({ message: 'Error when adding the reservation', err: err })
+        console.error(err)
+        return res.status(500).send({ message: 'Error when adding the reservation', err: err })
     }
-  }
+}
 
 //Editar reservacion
-export const editReserva = async(req, res) =>{
+export const editReserva = async (req, res) => {
     try {
         let data = req.body
-        let {id} = req.params
-        let update = checkUpdateReserva(data,id)
-        if(!update) return res.status(400).send({message: 'Have submitted some data that cannot be updated or missing data'})
+        let { id } = req.params
+        let update = checkUpdateReserva(data, id)
+        if (!update) return res.status(400).send({ message: 'Have submitted some data that cannot be updated or missing data' })
         let updateReserva = await Reser.findOneUpdate(
-            {_id: id},
-             data,
-            {new: true}
+            { _id: id },
+            data,
+            { new: true }
         )
-        if(!updateReserva) return res.status(404).send({message: 'Reservation not found and not upadate' })
+        if (!updateReserva) return res.status(404).send({ message: 'Reservation not found and not upadate' })
         return res.status(200).send({ message: 'Reservation edited successfully', updateReserva });
     } catch (err) {
         return res.status(500).send({ message: 'Error when editing the reservation', err: err });
@@ -50,12 +52,12 @@ export const editReserva = async(req, res) =>{
 export const cancelReserva = async (req, res) => {
     try {
         let { id } = req.params
-        let cancelReserva = await CSSMathProduct.findOneAndDelete({_id: id})
-        if(!cancelReserva) return res.status(404).send({message: 'Reservation not found and not deleted'})
-        return res.status(200).send({message: 'Reservation deleted successfully'})
-    }catch (err) {
+        let cancelReserva = await CSSMathProduct.findOneAndDelete({ _id: id })
+        if (!cancelReserva) return res.status(404).send({ message: 'Reservation not found and not deleted' })
+        return res.status(200).send({ message: 'Reservation deleted successfully' })
+    } catch (err) {
         console.error(err)
-        return res.status(500).send({message:'Error canceling the reservation'})
-    }    
+        return res.status(500).send({ message: 'Error canceling the reservation' })
+    }
 }
 
--- src/reservation/reservation.routes.js
+++ src/reservation/reservation.routes.js
@@ -2,10 +2,11 @@
 
 import { Router } from "express"
 import { addReserva, cancelReserva, editReserva } from "./reservation.controller.js"
+import { validateJwt } from "../middleware/validate-jwt.js"
 
 const api = Router()
 
-api.post('/new', addReserva)
+api.post('/new', validateJwt, addReserva)
 api.put('/edit/:id', editReserva)
 api.delete('/delete/:id', cancelReserva)
 
