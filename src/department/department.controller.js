06d0c7cde525a66c32adc3cc3db1989c162936d3
jboteo-2019465 <jboteo-2019465@kinal.edu.gt>
2024-05-13 17:16:49 -0600

    Creacion de departamento por defecto

--- src/department/department.controller.js
+++ src/department/department.controller.js
@@ -1,90 +1,105 @@
 'use strict'
 
 import Department from './department.model.js'
-import {checkUpdate} from '../utils/validator.js'
+import { checkUpdate } from '../utils/validator.js'
 
 
 //Registro de departamentos por defecto
-export const registerD = async (req, res)=>{
+export const registerD = async (req, res) => {
     try {
-        let data = req.body;
-        const existingDepartment = await Department.findOne({nameDepartment: data.nameDepartment})
-        if(existingDepartment){
-            return res.status(400).send({message: 'This department alredy exists'})
-        }
+        // Lista de nombres de departamentos
+        const departmentNames = [
+            'Guatemala', 'Peten', 'Quetzaltenango', 'Retalhuleu', 'Sacatepequez', 
+            'San Marcos', 'Solola', 'Totonicapan', 'Zacapa', 'Baja Verapaz', 
+            'Chimaltenango', 'Chiquimula', 'El Progreso', 'Huehuetenango', 
+            'Jalapa', 'Izabal', 'Jutiapa', 'Alta Verapaz', 'Quiche', 
+            'Suchitepequez', 'Santa Rosa', 'Escuintla'
+        ];
 
-        let department = new Department(data)
-        await department.save()
-        return res.send({message: 'The department has been registered'})
+        // Buscar los departamentos existentes en la base de datos
+        const existingDepartments = await Department.find({ nameDepartment: { $in: departmentNames } });
+
+        // Crear un conjunto de nombres de departamentos existentes
+        const existingDepartmentNames = new Set(existingDepartments.map(dep => dep.nameDepartment));
+
+        // Filtrar los nombres de departamentos que no existen y crearlos
+        const newDepartments = departmentNames.filter(name => !existingDepartmentNames.has(name));
+
+        // Crear los nuevos departamentos
+        const newDepartmentPromises = newDepartments.map(name => Department.create({ nameDepartment: name }));
+
+        // Esperar a que se creen los nuevos departamentos
+        await Promise.all(newDepartmentPromises);
+
+        console.log("Departamentos registrados correctamente.");
     } catch (err) {
-        console.error(err)
-        return res.status(500).send({message: 'Error registering the department'})
-        
+        console.error(err);
     }
 }
 
+
 //Obtener los departamentos
-export const obtener = async(req, res)=>{
+export const obtener = async (req, res) => {
     try {
         let data = await Department.find()
         return res.send(data)
     } catch (err) {
         console.error(err)
-        return res.status(500).send({message: 'Yhis information cannot be brought'})
+        return res.status(500).send({ message: 'Yhis information cannot be brought' })
     }
 }
 
 //Buscar individualmente los departamentos
-export const searchD = async(req,res)=>{
+export const searchD = async (req, res) => {
     try {
-        let {search} = req.body
-        let department = await Department.find({nameDepartment: {$regex: search}});
+        let { search } = req.body
+        let department = await Department.find({ nameDepartment: { $regex: search } });
 
-        if(!department){
-            return res.status(404).send({message: 'Department not found'})
+        if (!department) {
+            return res.status(404).send({ message: 'Department not found' })
         }
-        return res.send({message: 'Department Found', department})
+        return res.send({ message: 'Department Found', department })
     } catch (err) {
         console.error(err)
-        return res.status(500).send({message: 'Error searching Department'})
-        
+        return res.status(500).send({ message: 'Error searching Department' })
+
     }
 }
 
 //Actualizar un departamento
-export const updateD = async(req, res)=>{
+export const updateD = async (req, res) => {
     try {
-        let {id} = req.params
+        let { id } = req.params
         let data = req.body
         let update = checkUpdate(data, id)
-        if(!update) return res.status(400).send({message: 'Have submitted some data that cannot be update or missing data'})
+        if (!update) return res.status(400).send({ message: 'Have submitted some data that cannot be update or missing data' })
         let updateDepartment = await Department.findOneAndUpdate(
-            {_id: id},
+            { _id: id },
             data,
-            {new: true}
+            { new: true }
         )
-        if(!updateDepartment) return res.status(401).send({message: 'Department not found'})
-        return res.send({message: 'Updated department'})
+        if (!updateDepartment) return res.status(401).send({ message: 'Department not found' })
+        return res.send({ message: 'Updated department' })
     } catch (err) {
         console.error(err)
-        return res.status(500).send({message: 'Error updating'})
+        return res.status(500).send({ message: 'Error updating' })
     }
 }
 
 //Eliminar un departamento
-export const deleteD = async(req, res)=>{
+export const deleteD = async (req, res) => {
     try {
-        const {nameDepa} = req.params
+        const { nameDepa } = req.params
 
-        const deleteDepartment = await Department.findOneAndDelete({nameDepartment: nameDepa})
-        if(!deleteDepartment){
-            return res.status(404).send({message: 'Category not found and not delete'})
+        const deleteDepartment = await Department.findOneAndDelete({ nameDepartment: nameDepa })
+        if (!deleteDepartment) {
+            return res.status(404).send({ message: 'Category not found and not delete' })
         }
-        return res.send({message: `Department whit name ${deleteDepartment.nameDepartment} deleted successfully`})
+        return res.send({ message: `Department whit name ${deleteDepartment.nameDepartment} deleted successfully` })
     } catch (err) {
         console.error(err)
-        return res.status(500).send({message: 'Error deleting department'})
-        
+        return res.status(500).send({ message: 'Error deleting department' })
+
     }
 }
 
--- src/reservation/reservation.controller.js
+++ src/reservation/reservation.controller.js
@@ -10,16 +10,27 @@ export const addReserva = async (req, res) => {
         let user = req.user
         data.client = user._id
 
-        // Hace la reservacion
-        let newReserva = new Reser(data)
-        await newReserva.save()
-
         // Verifica si la habitacion esta disponible
         let room = await Room.findOne({ _id: data.room, available: 'DISPONIBLE' })
         if (!room) return res.status(404).send({ message: 'Room not found or not available' })
 
+        // Verifica si otro cliente está intentando reservar la misma habitación para la misma fecha
+        const otherClientReservation = await Reser.findOne({
+            room,
+            entryDate: { $gte: new Date(data.entryDate) },
+            client: { $ne: data.client } // Excluir la reserva del cliente actual
+        })
+
+        // Verifica que la fecha de vencimiento no sea anterior a la fecha de reserva
+        if (new Date(data.departureDate) < new Date(data.entryDate)) {
+            return res.status(400).json({ message: 'Expiration date cannot be before reservation date' })
+        }
+
         // Cambiar la disponibilidad de la habitacion
         room.available = 'NODISPONIBLE'
+        // Hace la reservacion
+        let newReserva = new Reser(data)
+        await newReserva.save()
         await room.save()
 
         return res.status(200).send({ message: 'Reservation added successfully', newReserva })
